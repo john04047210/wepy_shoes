e8dfd586026628dce9721f253fa46258f20b5952
qiaopeng <a1103.qiaop@neusoft.co.jp>
2020-01-06 19:37:37 +0900

    first commit

---
+++ wepy.config.js
@@ -0,0 +1,67 @@
+const LessPluginAutoPrefix = require('less-plugin-autoprefix')
+var prod = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  wpyExt: '.wpy',
+  eslint: true,
+  cliLogs: true,
+  compilers: {
+    less: {
+      compress: true,
+      plugins: [new LessPluginAutoPrefix({browsers: ['Android >= 2.3', 'Chrome > 20', 'iOS >= 6']})]
+    },
+    /*sass: {
+      outputStyle: 'compressed'
+    },*/
+    babel: {
+      sourceMap: true,
+      presets: [
+        'env'
+      ],
+      plugins: [
+        'babel-plugin-transform-class-properties',
+        'transform-export-extensions',
+        'syntax-export-extensions'
+      ]
+    }
+  },
+  plugins: {
+  },
+  appConfig: {
+    noPromiseAPI: ['createSelectorQuery']
+  }
+}
+
+if (prod) {
+
+  module.exports.cliLogs = false;
+
+  delete module.exports.compilers.babel.sourcesMap;
+  // 压缩sass
+  // module.exports.compilers['sass'] = {outputStyle: 'compressed'}
+
+  // 压缩less
+  module.exports.compilers['less'] = {
+    compress: true
+  }
+
+  // 压缩js
+  module.exports.plugins = {
+    uglifyjs: {
+      filter: /\.js$/,
+      config: {
+      }
+    },
+    imagemin: {
+      filter: /\.(jpg|png|jpeg)$/,
+      config: {
+        jpg: {
+          quality: 80
+        },
+        png: {
+          quality: 80
+        }
+      }
+    }
+  }
+}
